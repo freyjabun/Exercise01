1588f3bf6b29628778f4f6a550b4e9707a18ff10
Freyja <74057189+freyjabun@users.noreply.github.com>
2021-09-06 13:24:21 +0200

    Set up project, SLN file, and CI (#1)

---
+++ Exercise01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Exercise01
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
